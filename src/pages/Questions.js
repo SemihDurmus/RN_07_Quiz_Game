045a6362f52469733e4c3d1836e2b489265602bf
SemihDurmus <semihdurmus@yahoo.com>
2020-12-23 20:38:15 +0100

    Add: initial files

---
+++ src/pages/Questions.js
@@ -0,0 +1,24 @@
+import React, {useRef, useState} from 'react';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  Text,
+  Animated,
+  Button,
+} from 'react-native';
+//import {QuestionItem} from '../components';
+// import {useSelector, useDispatch} from 'react-redux';
+// import {CountdownCircleTimer} from 'react-native-countdown-circle-timer';
+
+const Questions = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1}}>
+        <Text>QUESTIONS</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export {Questions};
